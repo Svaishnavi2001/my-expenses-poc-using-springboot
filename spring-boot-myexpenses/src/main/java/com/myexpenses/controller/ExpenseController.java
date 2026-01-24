8c9f91279a70b899138af0d53072ae2956f0babd
Svaishnavi2001 <svaishnavi823@gmail.com>
2026-01-24 14:32:31 +0530

    Remove unnecessary blank lines in ExpenseController and ExpenseService

--- spring-boot-myexpenses/src/main/java/com/myexpenses/controller/ExpenseController.java
+++ spring-boot-myexpenses/src/main/java/com/myexpenses/controller/ExpenseController.java
@@ -5,7 +5,6 @@ import com.myexpenses.service.ExpenseService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
--- spring-boot-myexpenses/src/main/java/com/myexpenses/service/ExpenseService.java
+++ spring-boot-myexpenses/src/main/java/com/myexpenses/service/ExpenseService.java
@@ -5,7 +5,6 @@ import com.myexpenses.repository.ExpenseRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
